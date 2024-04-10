46140d235b4286f2d12c779d0cf08c0e2be660ad
Claudio Cruz <claudiocruz1989@gmail.com>
2024-04-10 19:35:59 +0200

    Unable users to add the same meal more then one.

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -10,48 +10,48 @@ function cartReducer(state, action) {
   if (action.type === 'ADD_ITEM') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
-    )
-
-    const updatedItems = [...state.items]
+    );
 
     if (existingCartItemIndex > -1) {
-      const existingItem = state.items[existingCartItemIndex]
+      const existingItem = state.items[existingCartItemIndex];
       const updatedItem = {
         ...existingItem,
         quantity: existingItem.quantity + 1
-      }
-      updatedItem[existingCartItemIndex] = updatedItem
-    } else {
-      updatedItems.push({ ...action.item, quantity: 1 })
-    }
+      };
 
-    return { ...state, items: updatedItems }
+      const updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+
+      return { ...state, items: updatedItems };
+    } else {
+      return { ...state, items: [...state.items, { ...action.item, quantity: 1 }] };
+    }
   }
 
   if (action.type === 'REMOVE_ITEM') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
-    )
-    const existingCartItem = state.items[existingCartItemIndex]
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
 
-    const updatedItems = [...state.items]
+    const updatedItems = [...state.items];
 
     if (existingCartItem.quantity === 1) {
-      
-      updatedItems.splice(existingCartItemIndex, 1)
+      updatedItems.splice(existingCartItemIndex, 1);
     } else {
       const updatedItem = {
         ...existingCartItem,
-        quantity: existingCartItem.quantity -1,
-      }
-      updatedItems[existingCartItemIndex] = updatedItem
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
     }
 
-    return { ...state, items: updatedItems }
+    return { ...state, items: updatedItems };
   }
 
-  return state
+  return state;
 }
+
 
 export function CartContextProvider({ children }) {
   const [ cart, dispatchCartAction ] = useReducer(cartReducer, { items: [] })
